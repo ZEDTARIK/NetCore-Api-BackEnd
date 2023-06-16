205e41b955057a0f2f2089ab5d6a8daaab3ce10a
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-06-16 16:48:39 +0100

    add defeinition code angular

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { DepartmentComponent } from './department/department.component';
 import { EmployeeComponent } from './employee/employee.component';
 
+/* This code defines the routes for an Angular application. It creates an array of `Routes` objects,
+where each object represents a route. */
 const appRoutes: Routes = [
   { path: '', component: DepartmentComponent},
   {path: 'department', redirectTo: '', component: EmployeeComponent},
